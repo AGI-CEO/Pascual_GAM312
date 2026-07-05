f9e8f85adef53a34c3b6e971a059cd93a71b5c18
Blaise Pascual <jetblaise@gmail.com>
2026-07-05 19:40:45 -0400

    feat: implement GameBase_GAM312 class and configure project engine settings

---
+++ Source/Pascual_GAM312/GameBase_GAM312.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameBase_GAM312.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PASCUAL_GAM312_API AGameBase_GAM312 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
